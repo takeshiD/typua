9e1d40b451fc678ce603a718f4d87d26624db0dc
takeshid <goriponikeike55@gmail.com>
2025-05-19 21:33:09 +0900

    feat: typecheck binary operator

--- src/typecheck.rs
+++ src/typecheck.rs
@@ -22,7 +22,7 @@ pub struct FunctionType {
 }
 
 pub fn typecheck(expr: &full_moon::ast::Expression) -> Result<Type, String> {
-    use full_moon::ast::Expression;
+    use full_moon::ast::{Expression, BinOp};
     use full_moon::tokenizer::TokenType;
     match &expr {
         Expression::Number(tknref) => match &tknref.token_type() {
@@ -53,9 +53,33 @@ pub fn typecheck(expr: &full_moon::ast::Expression) -> Result<Type, String> {
                 Err(err_string)
             }
         },
-        _ => panic!("Not Implemented"), // Expression::BinaryOperator { lhs, binop, rhs } => {},
-                                        // Expression::Parentheses { contained, expression } => {}
-                                        // Expression::UnaryOperator { unop, expression } => {}
+        Expression::BinaryOperator { lhs, binop, rhs } => {
+            let lhs_ty = typecheck(lhs.as_ref())?;
+            let rhs_ty = typecheck(rhs.as_ref())?;
+            match binop {
+                BinOp::Plus(_) | BinOp::Minus(_) => {
+                    if lhs_ty == rhs_ty {
+                        match lhs_ty {
+                            Type::Integer => Ok(Type::Integer),
+                            Type::Number => Ok(Type::Number),
+                            _ => {
+                                Err("Expected Arithmetic type. Got {}".to_string())
+                            },
+                        }
+                    } else {
+                        let err_string = format!("Different type, Got left is {:?}, right is {:?}.", lhs_ty, rhs_ty);
+                        Err(err_string)
+                    }
+                }
+                _ => Err("Not unimplemented".to_string())
+            }
+        }
+        // Expression::Parentheses {
+        //     contained,
+        //     expression,
+        // } => {}
+        // Expression::UnaryOperator { unop, expression } => {}
+        _ => unimplemented!()
     }
 }
 
@@ -63,17 +87,60 @@ pub fn typecheck(expr: &full_moon::ast::Expression) -> Result<Type, String> {
 mod tests {
     use super::*;
     use full_moon::ShortString;
-    use full_moon::ast::Expression;
-    use full_moon::tokenizer::{Token, TokenReference, TokenType};
+    use full_moon::ast::{Expression, BinOp};
+    use full_moon::tokenizer::{Token, TokenReference, TokenType, Symbol};
+
     #[test]
     fn test_number() {
-        let expr = Expression::Number(TokenReference::new(
+        let expr1 = Expression::Number(TokenReference::new(
             vec![],
             Token::new(TokenType::Number {
                 text: ShortString::new("1"),
             }),
             vec![],
         ));
-        assert_eq!(typecheck(&expr), Ok(Type::Number));
+        assert_eq!(typecheck(&expr1), Ok(Type::Number));
+        let expr1 = Expression::Number(TokenReference::new(
+            vec![],
+            Token::new(TokenType::Number {
+                text: ShortString::new("1"),
+            }),
+            vec![],
+        ));
+        assert_eq!(typecheck(&expr1), Ok(Type::Number));
+    }
+
+    #[test]
+    fn test_binaryop() {
+        let expr1 = Expression::BinaryOperator {
+            lhs: Box::new(
+                Expression::Number(TokenReference::new(
+                    vec![],
+                    Token::new(TokenType::Number {
+                        text: ShortString::new("1")
+                    }),
+                    vec![]
+                )
+            )),
+            binop: BinOp::Plus(TokenReference::new(
+                    vec![],
+                    Token::new(TokenType::Symbol {
+                        symbol: Symbol::Plus,
+                    }),
+                    vec![]
+                )
+            ),
+            rhs: Box::new(
+                Expression::Number(TokenReference::new(
+                    vec![],
+                    Token::new(TokenType::Number {
+                        text: ShortString::new("2")
+                    }),
+                    vec![]
+                )
+            )),
+        };
+        assert_eq!(typecheck(&expr1), Ok(Type::Number));
     }
 }
+
