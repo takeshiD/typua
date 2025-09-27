883f7c750be0e486d6f83cb9b8bad3713f1abbea
takeshid <goriponikeike55@gmail.com>
2025-09-27 09:59:09 +0900

    add test case

--- tests/scripts/class.lua
+++ tests/scripts/class.lua
@@ -3,15 +3,25 @@
 ---@field y string
 
 ---@type Container
+
 local c = {}
 
 ---@type Container
 local Container = {}
 
-
----@class (exact) Point
+---@class (exact) Point2D
 ---@field x number
 ---@field y number
-local Point = {}
-Point.x = 1
-Point.y = 2
+local p2d = {}
+p2d.x = 1
+p2d.y = 2
+p2d.z = 12
+
+---@class (exact) Point3D
+---@field x number
+---@field y number
+---@field z number
+local p3d = {}
+p3d.x = 1
+p3d.y = 2
+p3d.z = 2
